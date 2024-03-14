49ff19496e61d3c1cec06de679a85c4fd1e2e95c
CL <ongchangle03@gmail.com>
2024-03-14 23:38:32 +0800

    Add the Ui for the weather app

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,7 @@
 import "package:flutter/material.dart";
+import "package:lottie/lottie.dart";
+import "package:weather_app/models/weather_models.dart";
+import "package:weather_app/services/weather_service.dart";
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +11,84 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  // api key
+  final _weatherService = WeatherService("6f5c026c15ef4110ea6503a1b0f5ea51");
+  Weather? _weather;
+
+  // fetch weather
+  _fetchWeather() async {
+    // get the current city
+    String cityName = await _weatherService.getCurrentCity();
+
+    // get weather for the city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // weather animations
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return "assets/sunny.json";
+
+    switch (mainCondition) {
+      case "clouds":
+      case "mist":
+      case "smoke":
+      case "haze":
+      case "dust":
+      case "fog":
+        return "assets/cloud.json";
+      case "rain":
+      case "drizzle":
+      case "shower rain":
+        return "assets/rain.json";
+      case "thunderstorm":
+        return "assets/thunderstorm.json";
+      case "clear":
+        return "assets/sunny.json";
+      default:
+        return "assets/sunny.json";
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // fetch the weather
+    _fetchWeather();
+  }
+
+  // weather animations
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // city name
+            Text(_weather?.cityName ?? "loading city ..."),
+
+            // animation
+            Lottie.asset(
+              getWeatherAnimation(_weather?.mainCondition),
+            ),
+
+            // temperature
+            Text("${_weather?.temperature.round()}°C"),
+
+            // weather condition
+            Text(_weather?.mainCondition ?? ""),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -13,7 +13,7 @@ class WeatherService {
 
   Future<Weather> getWeather(String cityName) async {
     final response = await http
-        .get(Uri.parse('$BASE_URL?q=$cityName&appid-$apiKey&units=metric'));
+        .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
 
     if (response.statusCode == 200) {
       return Weather.fromJson(jsonDecode(response.body));
